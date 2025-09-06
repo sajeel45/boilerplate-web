5c7361cfd59ecad5e04633a1ad691fe4ddb177e5
Dev (Sajeel) <sheikhsajeel0@gmail.com>
2025-09-06 19:08:31 +0500

    feat: Enhance book listing with loading state and delete functionality

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -9,6 +9,7 @@ import {
 } from "../redux/pagesSlices/bookSlice";
 import { useSelector } from "react-redux";
 import { Spinner } from "react-bootstrap";
+import LoaderComponent from "../components/LoaderComponent";
 export default function Homepage() {
   const [page, setPage] = useState(1);
   const dispatch = useDispatch<AppDispatch>();
@@ -27,7 +28,7 @@ export default function Homepage() {
   const handlePageChange = (newPage: number) => {
     setPage(newPage);
   };
-  const loading = useSelector((s) => s?.books?.deleteBookAsyncThunk);
+  const loading = useSelector((s) => s?.books?.loadings?.getBooksAsyncThunk);
   const handleDeleteBook = (bookId: string) => {
     dispatch(
       deleteBookAsyncThunk({
@@ -57,35 +58,44 @@ export default function Homepage() {
           </div>
         </div>
         <div className="books-listing">
-          <table>
-            <thead>
-              <tr>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Genre</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.results?.map((item: any) => (
-                <tr key={item?._id || item?.id}>
-                  <td>{item?.title}</td>
-                  <td>{item?.author}</td>
-                  <td>{item?.genre}</td>
-                  <td>
-                    <button
-                      onClick={() => handleDeleteBook(item?._id || item?.id)}
-                      className="delete-button"
-                    >
-                      {" "}
-                      {loading ? <Spinner /> : "Delete"}
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <PaginationComponent data={data} onPageChange={handlePageChange} />
+          {loading ? (
+            <LoaderComponent />
+          ) : data?.results?.length === 0 ? (
+            <div style={{ textAlign: "center", padding: "2rem 0", color: "#6366f1", fontWeight: 500 }}>
+              No books found.
+            </div>
+          ) : (
+            <>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Genre</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data?.results?.map((item: any) => (
+                    <tr key={item?._id || item?.id}>
+                      <td>{item?.title}</td>
+                      <td>{item?.author}</td>
+                      <td>{item?.genre}</td>
+                      <td>
+                        <button
+                          onClick={() => handleDeleteBook(item?._id || item?.id)}
+                          className="delete-button"
+                        >
+                          {loading ? <Spinner /> : "Delete"}
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <PaginationComponent data={data} onPageChange={handlePageChange} />
+            </>
+          )}
         </div>
       </div>
     </div>
